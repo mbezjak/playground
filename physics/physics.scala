8ff9b9bddf6ccbf6e9444926dc183ec48c434f97
Miro Bezjak <bezjak.miro@gmail.com>
2013-01-12 22:42:59 +0100

    Physics constants to solve some problems easier.

---
+++ physics/physics.scala
@@ -0,0 +1,13 @@
+import math._
+
+val c  = 2.99792458e8     // m/s
+val AU = 1.49597870691e11 // m
+val G  = 6.673e-11        // m^3 kg^-1 s^-2
+val me = 9.109382e-31     // kg - electron mass
+val mp = 1.6726217e-27    // kg - proton mass
+val ma = 6.64465675e-27   // kg - alpha particle mass
+val Kb = 1.380648e-23     // J/K - boltzmann constant
+
+// astronomical
+val Msun = 1.9891e30    // kg
+val Rsun = 6.95508e8    // m
