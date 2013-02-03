ae58c9d757d4a2f5caa6edcb8d56a8c8f2fae13e
Miro Bezjak <bezjak.miro@gmail.com>
2013-02-03 12:31:55 +0100

    Neutron mass.

--- physics/physics.scala
+++ physics/physics.scala
@@ -13,6 +13,7 @@ val c  = 2.99792458e8     // m/s
 val G  = 6.673e-11        // m^3 kg^-1 s^-2
 val me = 9.109382e-31     // kg - electron mass
 val mp = 1.6726217e-27    // kg - proton mass
+val mn = 1.6749274e-27    // kg - neutron mass
 val ma = 6.64465675e-27   // kg - alpha particle mass
 val b  = 0.0028977685     // m K - wien's constant
 val kb = 1.380648e-23     // J/K - boltzmann constant
