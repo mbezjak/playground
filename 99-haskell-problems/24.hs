b8328691718544cfbdd54bf9808da8f1d2707b4c
Miro Bezjak <bezjak.miro@gmail.com>
2013-03-12 10:54:51 +0100

    99HP: initial but ugly impl of problem 24

---
+++ 99-haskell-problems/24.hs
@@ -0,0 +1,17 @@
+import System.Random
+
+removeAt :: Int -> [a] -> [a]
+removeAt _ []     = []
+removeAt 0 (_:xs) = xs
+removeAt n (x:xs) = x:removeAt (n-1) xs
+
+
+diff_select :: Int -> Int -> IO [Int]
+diff_select n m = getStdRandom (select n [1..m])
+
+select :: RandomGen g => Int -> [Int] -> g -> ([Int], g)
+select _ [] g             = ([], g)
+select 0 _  g             = ([], g)
+select remaining pool gen = (pool!!idx:rest, newGen')
+  where (idx, newGen)   = randomR (0, length pool - 1) gen
+        (rest, newGen') = select (remaining-1) (removeAt idx pool) newGen
