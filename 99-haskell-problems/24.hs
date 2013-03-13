3da92a62d4160f56b1833a9b32f95c2b6048bb22
Miro Bezjak <bezjak.miro@gmail.com>
2013-03-13 10:28:50 +0100

    99HP: problem 24 with State monad.
    Well that is basically the same as using randomRIO...

--- 99-haskell-problems/24.hs
+++ 99-haskell-problems/24.hs
@@ -1,4 +1,5 @@
 import System.Random
+import Control.Monad.State
 
 removeAt :: Int -> [a] -> [a]
 removeAt _ []     = []
@@ -15,3 +16,15 @@ select 0 _  g             = ([], g)
 select remaining pool gen = (pool!!idx:rest, newGen')
   where (idx, newGen)   = randomR (0, length pool - 1) gen
         (rest, newGen') = select (remaining-1) (removeAt idx pool) newGen
+
+
+diff_select' :: Int -> Int -> IO [Int]
+diff_select' n m = getStdRandom (select' n [1..m])
+
+select' :: RandomGen g => Int -> [Int] -> g -> ([Int], g)
+select' _ [] g           = ([], g)
+select' 0 _  g           = ([], g)
+select' remaining pool g = flip runState g $ do
+  idx  <- state $ randomR (0, length pool - 1)
+  rest <- state $ select' (remaining-1) (removeAt idx pool)
+  return $ pool!!idx:rest
