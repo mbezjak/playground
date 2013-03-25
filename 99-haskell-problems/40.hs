891485d7e07eefb83cf8b18a8979b1034d3d1481
Miro Bezjak <bezjak.miro@gmail.com>
2013-03-25 15:23:21 +0100

    99HP: working but slow impl of problem 40

---
+++ 99-haskell-problems/40.hs
@@ -0,0 +1,15 @@
+goldbach :: Int -> [Int]
+goldbach n = head . filter eq . combinations . primesR 1 $ n
+  where eq xs = sum xs == n
+
+primesR :: Int -> Int -> [Int]
+primesR a b = [ x | x <- [a..b], isPrime x]
+
+isPrime :: Int -> Bool
+isPrime x = all (\n -> x `mod` n /= 0) [2..limit]
+  where limit = floor . sqrt . fromIntegral $ x
+
+combinations :: [a] -> [[a]]
+combinations []     = []
+combinations [x]    = [[x]]
+combinations (x:xs) = map (x:) (combinations xs) ++ combinations xs
