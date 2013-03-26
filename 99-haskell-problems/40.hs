2461604a0cf0168530af3f9a5aead9bf96943bb3
Miro Bezjak <bezjak.miro@gmail.com>
2013-03-26 10:13:41 +0100

    99HP: problem 40

--- 99-haskell-problems/40.hs
+++ 99-haskell-problems/40.hs
@@ -2,6 +2,10 @@ goldbach :: Int -> [Int]
 goldbach n = head . filter eq . combinations . primesR 1 $ n
   where eq xs = sum xs == n
 
+goldbach' :: Int -> (Int, Int)
+goldbach' n = head [ (a, n-a) | a <- primesR 2 n, isPrime (n-a) ]
+
+
 primesR :: Int -> Int -> [Int]
 primesR a b = [ x | x <- [a..b], isPrime x]
 
