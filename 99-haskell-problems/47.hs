ca4b5b7b0fdadf35e9a52d176f90f8164e36c9c4
Miro Bezjak <bezjak.miro@gmail.com>
2013-03-28 17:32:58 +0100

    99HP: problem 47

---
+++ 99-haskell-problems/47.hs
@@ -0,0 +1,41 @@
+and' :: Bool -> Bool -> Bool
+and' True b = b
+and' _ _    = False
+infixl 3 `and'`
+
+or' :: Bool -> Bool -> Bool
+or' False b = b
+or' _ _     = True
+infixl 2 `or'`
+
+nand' :: Bool -> Bool -> Bool
+nand' a b = not' (and' a b)
+
+nor' :: Bool -> Bool -> Bool
+nor' a b = not' (or' a b)
+
+xor' :: Bool -> Bool -> Bool
+xor' a b = a /= b
+
+impl' :: Bool -> Bool -> Bool
+impl' a b = (not' a) `or'` b
+
+equ' :: Bool -> Bool -> Bool
+equ' a b = not' (xor' a b)
+
+not' :: Bool -> Bool
+not' True  = False
+not' False = True
+infixl 4 `not'`
+
+table :: (Bool -> Bool -> Bool) -> [(Bool,Bool,Bool)]
+table f = [ (a,b,f a b) | (a,b) <- truth2 ]
+
+printTable :: (Bool -> Bool -> Bool) -> IO ()
+printTable f = mapM_ (putStrLn . show) (table f)
+
+truth2 :: [(Bool,Bool)]
+truth2 = [ (False, False)
+         , (False, True)
+         , (True,  False)
+         , (True,  True) ]
