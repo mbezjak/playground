7cebee84e54497bddb061994f31e33018e12652b
Miro Bezjak <bezjak.miro@gmail.com>
2013-04-06 13:27:20 +0200

    99HP: make problem 56 test for symmetry

--- 99-haskell-problems/56.hs
+++ 99-haskell-problems/56.hs
@@ -7,5 +7,5 @@ symmetric (Branch _ l r) = mirror l r
 
 mirror :: Tree a -> Tree a -> Bool
 mirror Empty Empty = True
-mirror (Branch _ l1 r1) (Branch _ l2 r2) = mirror l1 l2 && mirror r1 r2
+mirror (Branch _ l1 r1) (Branch _ l2 r2) = mirror l1 r2 && mirror l2 r1
 mirror _ _         = False
