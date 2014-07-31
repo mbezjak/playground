da65a88dea215ee8da4e898440a88c03e1ab602b
Miro Bezjak <bezjak.miro@gmail.com>
2014-07-31 10:27:12 +0200

    4clojure 88: symmetric difference

---
+++ clojure/4clojure/088.clj
@@ -0,0 +1,7 @@
+(defn f [xs ys]
+  (clojure.set/union (clojure.set/difference xs ys) (clojure.set/difference ys xs)))
+
+(assert (= (f #{1 2 3 4 5 6} #{1 3 5 7}) #{2 4 6 7}))
+(assert (= (f #{:a :b :c} #{}) #{:a :b :c}))
+(assert (= (f #{} #{4 5 6}) #{4 5 6}))
+(assert (= (f #{[1 2] [2 3]} #{[2 3] [3 4]}) #{[1 2] [3 4]}))
