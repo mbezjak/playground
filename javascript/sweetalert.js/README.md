e9eb50d1561b256055dc4e0873c3ce8abac296ba
Miro Bezjak <bezjak.miro@gmail.com>
2014-11-26 15:38:36 +0100

    javascript/sweetalert.js: usage example

---
+++ javascript/sweetalert.js/README.md
@@ -0,0 +1,3 @@
+Refs:
+
+ * http://tristanedwards.me/sweetalert
---
+++ javascript/sweetalert.js/app.js
@@ -0,0 +1,10 @@
+function show() {
+    swal({
+        title : 'Job done',
+        text  : 'The requested job has been successfully completed',
+        type  : 'success',
+        timer : 5000,
+        confirmButtonText  : 'Close',
+        confirmButtonColor : '#E3E3E3'
+    });
+};
