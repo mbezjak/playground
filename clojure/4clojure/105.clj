8e8c7f9ac7280cd476a7cec7f3c3a7dfaba69339
Miro Bezjak <bezjak.miro@gmail.com>
2015-03-13 11:09:22 +0100

    4clojure: new implementation for 105 based on state machine and trampoline

--- clojure/4clojure/105.clj
+++ clojure/4clojure/105.clj
@@ -1,6 +1,6 @@
 (use '[clojure.test :only (is)])
 
-(defn to-map [coll]
+(defn to-map2 [coll]
   (letfn [(into-current-key [acc cur-key xs]
             (if (empty? xs)
               acc
@@ -14,6 +14,36 @@
                     (into-current-key new-acc cur-key rst))))))]
 
     (into-current-key {}  (first coll) coll)))
+
+
+
+(defn to-map [coll]
+  (letfn
+      [(assoc-empty [map kw]
+         (assoc map kw []))
+       (assoc-conj [map kw val]
+         (assoc map kw (conj (get map kw) val)))
+
+       ;; state transitions
+       (kw->slurp [map [kw & rst]]
+         (partial slurp-values (assoc-empty map kw) kw rst))
+       (slurp->kw [map coll]
+         (partial new-keyword map coll))
+       (slurp->slurp [map kw [val & rst]]
+         (partial slurp-values (assoc-conj map kw val) kw rst))
+
+       ;; state machine
+       (new-keyword [map coll]
+         (if (empty? coll)
+           map
+           (kw->slurp map coll)))
+       (slurp-values [map kw coll]
+         (cond
+           (empty? coll) map
+           (keyword? (first coll)) (slurp->kw map coll)
+           :else (slurp->slurp map kw coll)))]
+
+    (trampoline new-keyword {} coll)))
 
 (is (= {} (to-map [])))
 (is (= {:a [1]} (to-map [:a 1])))
