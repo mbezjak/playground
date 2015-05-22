a9534317cc9df25b642df3f24b113fb7bcb83691
Miro Bezjak <bezjak.miro@gmail.com>
2015-05-22 12:01:31 +0200

    4clojure: 101 - Levenshtein distance via matrix

--- clojure/4clojure/101.clj
+++ clojure/4clojure/101.clj
@@ -1,5 +1,7 @@
 (use '[clojure.test :only (is)])
 
+                                        ; https://en.wikipedia.org/wiki/Levenshtein_distance
+
 (defn lev-recursive [s len-s t len-t]
   (cond
     (zero? len-s) len-t
@@ -12,8 +14,77 @@
            (+ 1    (lev-recursive s len-s   t len-t-1))
            (+ cost (lev-recursive s len-s-1 t len-t-1))))))
 
-(defn levenshtein [s t]
+(defn levenshtein2 [s t]
   (lev-recursive s (count s) t (count t)))
+
+
+(defn lev-matrix
+  ([s m t n matrix] (lev-matrix s m t n matrix 1 1))
+  ([s m t n matrix j i]
+   (let [j-1 (dec j)
+         i-1 (dec i)
+         i+1 (inc i)]
+     (cond
+       (> j n) (get-in matrix [m n])
+       (> i m) (lev-matrix s m t n matrix (inc j) 1)
+       (= (get s i-1) (get t j-1)) (lev-matrix s m t n (assoc-in matrix [i j] (get-in matrix [i-1 j-1])) j i+1)
+       :else
+       (let [deletion     (inc (get-in matrix [i-1, j]))
+             insertion    (inc (get-in matrix [i,   j-1]))
+             substitution (inc (get-in matrix [i-1, j-1]))
+             min-action (min deletion insertion substitution)]
+         (lev-matrix s m t n (assoc-in matrix [i j] min-action) j i+1))))))
+
+(defn lev-initial-matrix [m n]
+  (vec (for [i (range (inc m))]
+         (vec (for [j (range (inc n))]
+                (cond
+                  (zero? i) j
+                  (zero? j) i
+                  :else 0))))))
+
+(is (= (lev-initial-matrix 3 3)
+       [[0 1 2 3]
+        [1 0 0 0]
+        [2 0 0 0]
+        [3 0 0 0]]))
+
+(defn levenshtein [s t]
+  (let [m (count s)
+        n (count t)
+        matrix (lev-initial-matrix m n)]
+    (lev-matrix s m t n matrix)))
+
+
+(defn levenshtein-4clojure [s t]
+  (letfn [(lev-initial-matrix [m n]
+            (vec (for [i (range (inc m))]
+                   (vec (for [j (range (inc n))]
+                          (cond
+                            (zero? i) j
+                            (zero? j) i
+                            :else 0))))))
+          (lev-matrix
+            ([s m t n matrix] (lev-matrix s m t n matrix 1 1))
+            ([s m t n matrix j i]
+             (let [j-1 (dec j)
+                   i-1 (dec i)
+                   i+1 (inc i)]
+               (cond
+                 (> j n) (get-in matrix [m n])
+                 (> i m) (lev-matrix s m t n matrix (inc j) 1)
+                 (= (get s i-1) (get t j-1)) (lev-matrix s m t n (assoc-in matrix [i j] (get-in matrix [i-1 j-1])) j i+1)
+                 :else
+                 (let [deletion     (inc (get-in matrix [i-1, j]))
+                       insertion    (inc (get-in matrix [i,   j-1]))
+                       substitution (inc (get-in matrix [i-1, j-1]))
+                       min-action (min deletion insertion substitution)]
+                   (lev-matrix s m t n (assoc-in matrix [i j] min-action) j i+1))))))]
+    (let [m (count s)
+          n (count t)
+          matrix (lev-initial-matrix m n)]
+      (lev-matrix s m t n matrix))))
+
 
 (is (= (levenshtein "kitten" "sitting") 3))
 (is (= (levenshtein "closure" "clojure") (levenshtein "clojure" "closure") 1))
