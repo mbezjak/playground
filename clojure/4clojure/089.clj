848c7f73b7a9e8cb2bfdf56036a0e68b28eee1ba
Miro Bezjak <bezjak.miro@gmail.com>
2015-06-03 11:49:19 +0200

    4clojure: 89 - graph tour

--- clojure/4clojure/089.clj
+++ clojure/4clojure/089.clj
@@ -1,45 +1,5 @@
 (use '[clojure.test :only (is)])
 
-(defn all-nodes-in [graph]
-  (set (flatten graph)))
-
-(is (= #{:a :b :c} (all-nodes-in [[:a :a] [:b :b] [:a :c]])))
-
-(defn graph-to-map [graph]
-  (apply merge-with concat (for [[key val] graph] {key [val]})))
-
-(is (= (graph-to-map [[:a :a] [:a :b] [:b :c]])
-       {:a [:a :b]
-        :b [:c]}))
-
-(defn remove-visited-node [node graph-hash]
-  (->> (dissoc graph-hash node)
-       (map (fn [[key links]] {key (remove #{node} links)}))
-       (apply merge)
-       (filter (fn [[key links]] (not (empty? links))))
-       (into {})))
-
-(is (= (remove-visited-node :a {:a [:a :b] :b [:a] :c [:b]})
-       {:c [:b]}))
-
-(defn remove-visited-edge [[from to] graph-hash]
-  (let [rm-link (fn [key val map]
-                  (let [links (get map key)]
-                    (if links
-                      (assoc map key (remove #{val} links))
-                      map)))]
-    (->> graph-hash
-         (rm-link from to)
-         (rm-link to from)
-         (filter (fn [[key links]] (not (empty? links))))
-         (into {}))))
-
-(is (= (remove-visited-edge [:a :a] {:a [:a :b] :b [:a] :c [:b]})
-       {:a [:b] :b [:a] :c [:b]}))
-
-(is (= (remove-visited-edge [:a :b] {:a [:a :b] :b [:a] :c [:b]})
-       {:a [:a] :c [:b]}))
-
 (defn find-next-edges [graph node]
   (set (concat
         (filter #(= node (first %)) graph)
@@ -49,27 +9,56 @@
 (is (= #{[:a :b] [:a :c] (find-next-edges [[:a :b] [:b :c] [:a :c]] :a)}))
 (is (= #{[:b :a] [:b :c] (find-next-edges [[:a :b] [:b :c] [:a :c]] :b)}))
 
-(defn consume [[from to :as edge] remaining]
+(defn remove-from-graph [[from to] graph]
+  (remove #{[from to] [to from]} graph))
+
+(is (= [[:a :c]] (remove-from-graph [:a :b] [[:a :b] [:b :a] [:a :c]])))
+
+(defn consume [[from to] remaining]
   (if (empty? remaining)
     true
     (let [next-edges (find-next-edges remaining to)]
       (if (empty? next-edges)
         false
-        (not-every? false? (map (fn [next-edge]
-                                  (consume next-edge (remove #{next-edge} remaining)))
-                                next-edges))))))
+        (not (empty? (drop-while false? (map (fn [next-edge]
+                                               (consume next-edge (remove-from-graph next-edge remaining)))
+                                             next-edges))))))))
 
 (is (= true  (consume [:a :b] [])))
 (is (= false (consume [:a :b] [[:c :d]])))
 (is (= true  (consume [:a :b] [[:b :c]])))
 (is (= false (consume [:a :b] [[:b :c] [:b :d]])))
 (is (= true  (consume [:a :b] [[:b :c] [:c :d] [:b :d]])))
+(is (= true  (consume [:d :f] [[:a :b] [:a :c] [:c :b] [:a :e]
+                               [:b :e] [:a :d] [:b :d] [:c :e]
+                               [:d :e] [:c :f]])))
 
 (defn tour? [graph]
   (if-not (apply distinct? graph)
     false
-    (not-every? false? (for [starting-edge graph]
-                         (consume starting-edge (remove #{starting-edge} graph))))))
+    (not (empty? (drop-while false? (for [starting-edge graph]
+                                      (consume starting-edge (remove #{starting-edge} graph))))))))
+
+(defn tour-for-4clojure? [orig-graph]
+  (letfn [(find-next-edges [graph node]
+            (set (concat
+                  (filter #(= node (first %)) graph)
+                  (filter #(= node (first %)) (map reverse graph)))))
+          (remove-from-graph [[from to] graph]
+            (remove #{[from to] [to from]} graph))
+          (consume [[from to] remaining]
+            (if (empty? remaining)
+              true
+              (let [next-edges (find-next-edges remaining to)]
+                (if (empty? next-edges)
+                  false
+                  (not (empty? (drop-while false? (map (fn [next-edge]
+                                                         (consume next-edge (remove-from-graph next-edge remaining)))
+                                                       next-edges))))))))]
+    (if-not (apply distinct? orig-graph)
+      false
+      (not (empty? (drop-while false? (for [starting-edge orig-graph]
+                                        (consume starting-edge (remove #{starting-edge} orig-graph)))))))))
 
 (is (= true (tour? [[:a :b]])))
 
