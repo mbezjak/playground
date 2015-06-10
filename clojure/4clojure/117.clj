32e1fee52e7d150250a795abf5c64aa8b157df02
Miro Bezjak <bezjak.miro@gmail.com>
2015-06-10 10:12:02 +0200

    4clojure: 117 - for science

--- clojure/4clojure/117.clj
+++ clojure/4clojure/117.clj
@@ -58,8 +58,66 @@
 
 (is (= [1 1] (starting-position sample-maze)))
 
-(defn solvable? [maze])
-ee
+(defn goal-in-frontier? [maze frontier]
+  (some #(= \C %) (map #(get-in maze %) frontier)))
+
+(is (= nil (goal-in-frontier? sample-maze #{[1 2]})))
+(is (= true  (goal-in-frontier? sample-maze #{[1 3]})))
+
+(defn try-to-solve [maze frontier visited]
+  (cond
+    (empty? frontier) false
+    (goal-in-frontier? maze frontier) true
+    :else
+    (let [new-frontier (next-frontier maze frontier visited)
+          new-visited (clojure.set/union visited frontier)]
+      (try-to-solve maze new-frontier new-visited))))
+
+(defn solvable? [maze]
+  (try-to-solve maze #{(starting-position maze)} #{}))
+
+(defn solvable?-4clojure [maze]
+  (letfn [(movements-from-point [[x y]]
+            #{[(dec x) y]
+              [(inc x) y]
+              [x (dec y)]
+              [x (inc y)]})
+          (out-of-bounds? [[x y]]
+            (let [rows (count maze)
+                  cols (if (zero? rows) 0 (count (first maze)))]
+              (or
+               (< x 0)
+               (< y 0)
+               (>= x rows)
+               (>= y cols))))
+          (free-to-walk [point]
+            (not= \# (get-in maze point)))
+          (next-frontier [current-frontier visited]
+            (let [potential-movements (mapcat movements-from-point current-frontier)
+                  walkable-movements (filter #(and (not (out-of-bounds? %))
+                                                   (free-to-walk %))
+                                             potential-movements)]
+              (clojure.set/difference (set walkable-movements) visited)))
+          (starting-position []
+            (first
+             (for [[ridx row] (map-indexed vector maze)
+                   [cidx tile] (map-indexed vector row)
+                   :when (= tile \M)]
+               [ridx cidx])))
+          (goal-in-frontier? [frontier]
+            (some #(= \C %) (map #(get-in maze %) frontier)))
+          (try-to-solve [frontier visited]
+            (cond
+              (empty? frontier) false
+              (goal-in-frontier? frontier) true
+              :else
+              (let [new-frontier (next-frontier frontier visited)
+                    new-visited (clojure.set/union visited frontier)]
+                (try-to-solve new-frontier new-visited))))]
+
+    (try-to-solve #{(starting-position)} #{})))
+
+
 (is (= true  (solvable? ["M   C"])))
 
 (is (= false (solvable? ["M # C"])))
