29e36b35d20b19937b8a88fec432a82336d245e6
Miro Bezjak <bezjak.miro@gmail.com>
2015-07-06 10:33:59 +0200

    javascript/bacon.js: incorporate permissions

--- javascript/bacon.js/main.js
+++ javascript/bacon.js/main.js
@@ -3,35 +3,35 @@ Ext.onReady(function() {
     Ext.state.Manager.setProvider(Ext.create('Ext.state.LocalStorageProvider'));
 
     var sampleData = [
-        ['3m Co',                               71.72, 0.02,  0.03,  '9/1 12:00am'],
-        ['Alcoa Inc',                           29.01, 0.42,  1.47,  '9/1 12:00am'],
-        ['Altria Group Inc',                    83.81, 0.28,  0.34,  '9/1 12:00am'],
-        ['American Express Company',            52.55, 0.01,  0.02,  '9/1 12:00am'],
-        ['American International Group, Inc.',  64.13, 0.31,  0.49,  '9/1 12:00am'],
-        ['AT&T Inc.',                           31.61, -0.48, -1.54, '9/1 12:00am'],
-        ['Boeing Co.',                          75.43, 0.53,  0.71,  '9/1 12:00am'],
-        ['Caterpillar Inc.',                    67.27, 0.92,  1.39,  '9/1 12:00am'],
-        ['Citigroup, Inc.',                     49.37, 0.02,  0.04,  '9/1 12:00am'],
-        ['E.I. du Pont de Nemours and Company', 40.48, 0.51,  1.28,  '9/1 12:00am'],
-        ['Exxon Mobil Corp',                    68.1,  -0.43, -0.64, '9/1 12:00am'],
-        ['General Electric Company',            34.14, -0.08, -0.23, '9/1 12:00am'],
-        ['General Motors Corporation',          30.27, 1.09,  3.74,  '9/1 12:00am'],
-        ['Hewlett-Packard Co.',                 36.53, -0.03, -0.08, '9/1 12:00am'],
-        ['Honeywell Intl Inc',                  38.77, 0.05,  0.13,  '9/1 12:00am'],
-        ['Intel Corporation',                   19.88, 0.31,  1.58,  '9/1 12:00am'],
-        ['International Business Machines',     81.41, 0.44,  0.54,  '9/1 12:00am'],
-        ['Johnson & Johnson',                   64.72, 0.06,  0.09,  '9/1 12:00am'],
-        ['JP Morgan & Chase & Co',              45.73, 0.07,  0.15,  '9/1 12:00am'],
-        ['McDonald\'s Corporation',             36.76, 0.86,  2.40,  '9/1 12:00am'],
-        ['Merck & Co., Inc.',                   40.96, 0.41,  1.01,  '9/1 12:00am'],
-        ['Microsoft Corporation',               25.84, 0.14,  0.54,  '9/1 12:00am'],
-        ['Pfizer Inc',                          27.96, 0.4,   1.45,  '9/1 12:00am'],
-        ['The Coca-Cola Company',               45.07, 0.26,  0.58,  '9/1 12:00am'],
-        ['The Home Depot, Inc.',                34.64, 0.35,  1.02,  '9/1 12:00am'],
-        ['The Procter & Gamble Company',        61.91, 0.01,  0.02,  '9/1 12:00am'],
-        ['United Technologies Corporation',     63.26, 0.55,  0.88,  '9/1 12:00am'],
-        ['Verizon Communications',              35.57, 0.39,  1.11,  '9/1 12:00am'],
-        ['Wal-Mart Stores, Inc.',               45.45, 0.73,  1.63,  '9/1 12:00am']
+        ['3m Co',                               71.72, 0.02,  0.03,  '9/1 12:00am', 'user'],
+        ['Alcoa Inc',                           29.01, 0.42,  1.47,  '9/1 12:00am', 'user'],
+        ['Altria Group Inc',                    83.81, 0.28,  0.34,  '9/1 12:00am', 'user'],
+        ['American Express Company',            52.55, 0.01,  0.02,  '9/1 12:00am', 'user'],
+        ['American International Group, Inc.',  64.13, 0.31,  0.49,  '9/1 12:00am', 'user'],
+        ['AT&T Inc.',                           31.61, -0.48, -1.54, '9/1 12:00am', 'user'],
+        ['Boeing Co.',                          75.43, 0.53,  0.71,  '9/1 12:00am', 'user'],
+        ['Caterpillar Inc.',                    67.27, 0.92,  1.39,  '9/1 12:00am', 'admin'],
+        ['Citigroup, Inc.',                     49.37, 0.02,  0.04,  '9/1 12:00am', 'admin'],
+        ['E.I. du Pont de Nemours and Company', 40.48, 0.51,  1.28,  '9/1 12:00am', 'admin'],
+        ['Exxon Mobil Corp',                    68.1,  -0.43, -0.64, '9/1 12:00am', 'admin'],
+        ['General Electric Company',            34.14, -0.08, -0.23, '9/1 12:00am', 'admin'],
+        ['General Motors Corporation',          30.27, 1.09,  3.74,  '9/1 12:00am', 'admin'],
+        ['Hewlett-Packard Co.',                 36.53, -0.03, -0.08, '9/1 12:00am', 'admin'],
+        ['Honeywell Intl Inc',                  38.77, 0.05,  0.13,  '9/1 12:00am', 'admin'],
+        ['Intel Corporation',                   19.88, 0.31,  1.58,  '9/1 12:00am', 'admin'],
+        ['International Business Machines',     81.41, 0.44,  0.54,  '9/1 12:00am', 'admin'],
+        ['Johnson & Johnson',                   64.72, 0.06,  0.09,  '9/1 12:00am', 'admin'],
+        ['JP Morgan & Chase & Co',              45.73, 0.07,  0.15,  '9/1 12:00am', 'admin'],
+        ['McDonald\'s Corporation',             36.76, 0.86,  2.40,  '9/1 12:00am', 'admin'],
+        ['Merck & Co., Inc.',                   40.96, 0.41,  1.01,  '9/1 12:00am', 'admin'],
+        ['Microsoft Corporation',               25.84, 0.14,  0.54,  '9/1 12:00am', 'admin'],
+        ['Pfizer Inc',                          27.96, 0.4,   1.45,  '9/1 12:00am', 'admin'],
+        ['The Coca-Cola Company',               45.07, 0.26,  0.58,  '9/1 12:00am', 'admin'],
+        ['The Home Depot, Inc.',                34.64, 0.35,  1.02,  '9/1 12:00am', 'admin'],
+        ['The Procter & Gamble Company',        61.91, 0.01,  0.02,  '9/1 12:00am', 'admin'],
+        ['United Technologies Corporation',     63.26, 0.55,  0.88,  '9/1 12:00am', 'admin'],
+        ['Verizon Communications',              35.57, 0.39,  1.11,  '9/1 12:00am', 'admin'],
+        ['Wal-Mart Stores, Inc.',               45.45, 0.73,  1.63,  '9/1 12:00am', 'admin']
     ];
 
     /**
@@ -66,7 +66,8 @@ Ext.onReady(function() {
             {name: 'price',      type: 'float'},
             {name: 'change',     type: 'float'},
             {name: 'pctChange',  type: 'float'},
-            {name: 'lastChange', type: 'date', dateFormat: 'n/j h:ia'}
+            {name: 'lastChange', type: 'date', dateFormat: 'n/j h:ia'},
+            {name: 'permission'}
         ],
         data: sampleData
     });
@@ -192,8 +193,7 @@ Ext.onReady(function() {
         return record;
     });
     var selectedRecordUpdated = updated.filter(function(record) {
-        throw new Error('bal');
-        return record == grid.getSelectionModel().getSelection()[0];
+        return record === grid.getSelectionModel().getSelection()[0];
     });
 
     var selectionChange = Bacon.fromEvent(grid.getSelectionModel(), 'selectionchange', function(ignored, records) {
@@ -203,9 +203,20 @@ Ext.onReady(function() {
     var hasSelection = selectionChange.not().not();
     var onSelected = selectionChange.filter(Ext.identityFn);
     var moreThan40 = onSelected.merge(selectedRecordUpdated).map(function(record) { return record.get('price') > 40; });
-    var notSelectedOrMoreThan40 = hasSelection.combine(moreThan40, function(hasSelection, gt40) {
-        return hasSelection ? gt40 : false;
+
+    var permissionUserHas = 'user';
+    var recordPermission = selectionChange.filter(Ext.identityFn).map(function(record) {
+        return record.get('permission');
+    });
+    var permissionStream = Bacon.constant(permissionUserHas).sampledBy(recordPermission, function(userPermission, rowPermission) {
+        return userPermission === rowPermission;
     });
 
-    notSelectedOrMoreThan40.onValue(enableDisableInfoButton);
+    var hasPermissionAndSelectedAndMoreThan40 = hasSelection.combine(moreThan40, function(hasSelection, gt40) {
+        return hasSelection ? gt40 : false;
+    }).combine(permissionStream, function(selectedAndgt40, validPermission) {
+        return validPermission ? selectedAndgt40 : false;
+    });
+
+    hasPermissionAndSelectedAndMoreThan40.onValue(enableDisableInfoButton);
 });
