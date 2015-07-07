e3e49357ceb3a8526479ff770b39bb8de0f9cd33
Miro Bezjak <bezjak.miro@gmail.com>
2015-07-07 11:33:44 +0200

    javascript/bacon.js: other buttons also work with streams

--- javascript/bacon.js/main.js
+++ javascript/bacon.js/main.js
@@ -47,57 +47,50 @@ Ext.onReady(function() {
         viewConfig : { stripeRows : true },
         store      : store,
         columns    : [{
-            text      : 'Company',
-            flex      : 1,
-            sortable  : true,
-            dataIndex : 'company'
+            text         : 'Company',
+            flex         : 1,
+            sortable     : false,
+            resizable    : false,
+            draggable    : false,
+            menuDisabled : true,
+            dataIndex    : 'company'
         }, {
-            text      : 'Price',
-            width     : 75,
-            sortable  : true,
-            renderer  : 'usMoney',
-            dataIndex : 'price'
+            text         : 'Price',
+            width        : 75,
+            sortable     : false,
+            resizable    : false,
+            draggable    : false,
+            menuDisabled : true,
+            renderer     : 'usMoney',
+            dataIndex    : 'price'
         }]
     });
 
-    var removeAllButton = Ext.create('Ext.Button', {
-        text: "Remove all",
-        renderTo: document.body
-    });
-    var addBackButton = Ext.create('Ext.Button', {
-        text: 'Add back',
-        disabled: true,
-        renderTo: document.body,
-        handler: function() {
-            store.add(sampleData);
-        }
-    });
-
-    var change3mCoButton = Ext.create('Ext.Button', {
-        text: 'Change 3m Co',
-        renderTo: document.body,
-        handler: function() {
-            store.getAt(0).set('price', 20.01);
-        }
-    });
-
-    var infoButton = Ext.create('Ext.Button', {
-        text: 'Info',
-        disabled: true,
-        renderTo: document.body,
-        handler: function() {
-            var record = grid.getSelectionModel().getSelection()[0];
-            Ext.Msg.alert('Info', 'Price: ' + record.get('price'));
-        }
-    });
-
     // actions
-    var removeAll = function() {
-        store.removeAll();
+    var removeAll  = function() { store.removeAll(); };
+    var addBack    = function() { store.add(sampleData); };
+    var change3mCo = function() { store.getAt(0).set('price', 20.01); };
+    var info       = function() {
+        var record = grid.getSelectionModel().getSelection()[0];
+        Ext.Msg.alert('Info', 'Price: ' + record.get('price'));
     };
+
+    // buttons
+    var mkButton = function(text, disabled) {
+        return Ext.create('Ext.Button', {
+            text     : text,
+            disabled : disabled,
+            renderTo : document.body
+        });
+    };
+    var removeAllButton  = mkButton('Remove all', false);
+    var addBackButton    = mkButton('Add back', true);
+    var change3mCoButton = mkButton('Change 3m Co', false);
+    var infoButton       = mkButton('Info', true);
 
     // fn utils
     var constant   = function(val) { return function() { return val; }; };
+    var not        = function(fn) { return function() { return !fn.apply(null, arguments); }; };
     var secondArg  = function(fst, snd) { return snd; };
     var arrayFirst = function(array) { return array[0]; };
     var truthy     = function(val) { return !!val; };
@@ -117,6 +110,7 @@ Ext.onReady(function() {
             button.setDisabled(!enable);
         };
     };
+    var focusGrid = function() { grid.focus(); };
 
     // stream construction
     var streamFromClick    = function(button) { return Bacon.fromEvent(button, 'click'); };
@@ -127,6 +121,9 @@ Ext.onReady(function() {
     var dataChangedStream  = Bacon.fromEvent(store, 'datachanged', constant(store));
     var keyDownStream      = Bacon.fromEvent(grid.getEl(), 'keypress');
     var removeAllClicks    = streamFromClick(removeAllButton);
+    var addBackClicks      = streamFromClick(addBackButton);
+    var change3mCoClicks   = streamFromClick(change3mCoButton);
+    var infoClicks         = streamFromClick(infoButton);
 
     // stream usage
     var oneRowSelected       = multiSelectStream.map(isArrayOfOneElement);
@@ -137,7 +134,9 @@ Ext.onReady(function() {
     var hasValidPermission   = permissionProperty.sampledBy(permissionOfSelected, permissionsMatch);
     var storeHasSomething    = dataChangedStream.map(storeHasRecords);
     var deleteKey            = keyDownStream.filter(isKey(Ext.EventObject.DELETE));
+    var enterKey             = keyDownStream.filter(isKey(Ext.EventObject.ENTER));
     var removeAllEvents      = deleteKey.merge(removeAllClicks).map(store).filter(storeHasRecords);
+    var addBackEvents        = enterKey.merge(addBackClicks).map(store).filter(not(storeHasRecords));
 
     var enableDisableStream  = Bacon.combineWith(function(somethingSelected, isValidPermission, isGt40) {
         return somethingSelected && isValidPermission && isGt40;
@@ -148,4 +147,10 @@ Ext.onReady(function() {
     storeHasSomething.onValue(enableDisableButton(removeAllButton));
     storeHasSomething.not().onValue(enableDisableButton(addBackButton));
     removeAllEvents.onValue(removeAll);
+    addBackEvents.onValue(addBack);
+    change3mCoClicks.onValue(change3mCo);
+    infoClicks.onValue(info);
+    [removeAllEvents, addBackEvents, change3mCoClicks, infoClicks].forEach(function(stream) {
+        stream.onValue(focusGrid);
+    });
 });
