aa299107db8f59c4446168fc0144f0d1373e9754
Miro Bezjak <bezjak.miro@gmail.com>
2015-08-21 10:49:17 +0200

    4clojure: 125 - Gus' Quinundrum

--- clojure/4clojure/125.clj
+++ clojure/4clojure/125.clj
@@ -1,8 +1,64 @@
 (use '[clojure.test :only (is)])
+(require 'clojure.string)
 
-(println (str '((fn []
-                  (let [q \"
-                        l ["(fn []"
-                           "(let [q \\\""]])))))
+(println ((fn []
+            (let [q (char 34)
+                  s (char 32)
+                  l ["(fn []"
+                     "(let [q (char 34)"
+                     "s (char 32)"
+                     "l ["
+                     "]]"
+                     "(str (clojure.string/join s (take 4 l))"
+                     "(clojure.string/join s (map (fn [x] (str q x q)) l))"
+                     "(clojure.string/join s (drop 4 l)))))"]]
+              (str (clojure.string/join s (take 4 l))
+                   (clojure.string/join s (map (fn [x] (str q x q)) l))
+                   (clojure.string/join s (drop 4 l)))))))
 
-(is (= (str 'gus) (gus)))
+(println)
+
+(println (str '(fn []
+                 (let [q (char 34)
+                       s (char 32)
+                       l ["(fn []"
+                          "(let [q (char 34)"
+                          "s (char 32)"
+                          "l ["
+                          "]]"
+                          "(str (clojure.string/join s (take 4 l))"
+                          "(clojure.string/join s (map (fn [x] (str q x q)) l))"
+                          "(clojure.string/join s (drop 4 l)))))"]]
+                   (str (clojure.string/join s (take 4 l))
+                        (clojure.string/join s (map (fn [x] (str q x q)) l))
+                        (clojure.string/join s (drop 4 l)))))))
+
+(is (= (str '(fn []
+               (let [q (char 34)
+                     s (char 32)
+                     l ["(fn []"
+                        "(let [q (char 34)"
+                        "s (char 32)"
+                        "l ["
+                        "]]"
+                        "(str (clojure.string/join s (take 4 l))"
+                        "(clojure.string/join s (map (fn [x] (str q x q)) l))"
+                        "(clojure.string/join s (drop 4 l)))))"]]
+                 (str (clojure.string/join s (take 4 l))
+                      (clojure.string/join s (map (fn [x] (str q x q)) l))
+                      (clojure.string/join s (drop 4 l))))))
+
+       ((fn []
+          (let [q (char 34)
+                s (char 32)
+                l ["(fn []"
+                   "(let [q (char 34)"
+                   "s (char 32)"
+                   "l ["
+                   "]]"
+                   "(str (clojure.string/join s (take 4 l))"
+                   "(clojure.string/join s (map (fn [x] (str q x q)) l))"
+                   "(clojure.string/join s (drop 4 l)))))"]]
+            (str (clojure.string/join s (take 4 l))
+                 (clojure.string/join s (map (fn [x] (str q x q)) l))
+                 (clojure.string/join s (drop 4 l))))))))
