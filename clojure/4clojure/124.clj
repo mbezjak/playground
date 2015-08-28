4fe3979bbdfd3fe932ce1af612651e2fe1cc00a9
Miro Bezjak <bezjak.miro@gmail.com>
2015-08-28 11:02:59 +0200

    4clojure: 124 - analyze reversi

--- clojure/4clojure/124.clj
+++ clojure/4clojure/124.clj
@@ -1,5 +1,4 @@
 (use '[clojure.test :only (is)])
-(require 'clojure.pprint)
 (require 'clojure.set)
 
 (defn opposite-of [color]
@@ -24,17 +23,6 @@
 (is (= true  (valid? '[w b e])))
 (is (= true  (valid? '[e w b e])))
 (is (= true  (valid? '[w b b e])))
-
-(defn truncate [move]
-  (->> move
-       (reverse)
-       (drop-while #(= % 'e))
-       (cons 'e)
-       (reverse)
-       (vec)))
-
-(is (= '[w b e] (truncate '[w b e e])))
-(is (= '[w b e] (truncate '[w b e])))
 
 (defn color-positions [color board]
   (set
@@ -111,6 +99,13 @@
                             [e e e e]
                             [e e e e]])))
 
+(is (= '#{[(e [1 0]) (w [1 1]) (b [1 2]) (e [1 3])]
+          [(e [0 1]) (w [1 1]) (b [2 1]) (e [3 1])]}
+       (valid-moves [1 1] '[[e e e e]
+                            [e w b e]
+                            [e b w e]
+                            [e e e e]])))
+
 (defn positions-of-flips [move color]
   (->> move
        (filter (comp #(and (not= % 'e) (not= % color)) first))
@@ -121,15 +116,18 @@
        (positions-of-flips '[(e [3 0]) (w [2 1]) (b [1 2]) (e [0 3])] 'w)))
 
 (defn jump-position [move color]
-  (->> move
-       (drop-while (comp #(not= % color) first))
-       (rest)
-       (drop-while (comp #(not= % 'e) first))
-       (first)
-       (second)))
+  (let [move-str (clojure.string/join "" (map first move))
+        pattern-fwd (re-pattern (str "e*" color "+" (opposite-of color) "+e+"))
+        pattern-bck (re-pattern (str "e*" (opposite-of color) "+" color "+e+"))]
+    (if (re-matches pattern-fwd move-str)
+      (last (last move))
+      (last (first move)))))
 
 (is (= [0 3]
        (jump-position '[(e [3 0]) (w [2 1]) (b [1 2]) (e [0 3])] 'w)))
+
+(is (= [3 0]
+       (jump-position '[(e [3 0]) (b [2 1]) (w [1 2]) (e [0 3])] 'w)))
 
 (defn analyze [board color]
   (apply merge-with clojure.set/union
@@ -137,18 +135,72 @@
                move (valid-moves color-pos board)]
            {(jump-position move color) (positions-of-flips move color)})))
 
-(clojure.pprint/pprint
- (analyze '[[e e e e]
-            [e w b e]
-            [e b w e]
-            [e e e e]] 'w))
+(defn analyze-4clojure [board color]
+  (letfn [(opposite-of [color]
+            (if (= color 'w) 'b 'w))
+          (valid? [move]
+            (let [move-str (clojure.string/join "" move)]
+              (boolean (or (re-matches #"e*w+b+e+" move-str)
+                           (re-matches #"e*b+w+e+" move-str)))))
+          (color-positions [color board]
+            (set
+             (for [[ridx row] (map-indexed vector board)
+                   [cidx cell] (map-indexed vector row)
+                   :when (= cell color)]
+               [ridx cidx])))
+          (in-bounds? [[ridx cidx]]
+            (and (>= ridx 0)
+                 (>= cidx 0)
+                 (< ridx 4)
+                 (< cidx 4)))
+          (diagonal-down-move [start-pos board]
+            (let [up-left-pos (last (take-while in-bounds? (iterate #(mapv dec %) start-pos)))]
+              (vec (for [i (range 4)
+                         :let [pos (mapv #(+ % i) up-left-pos)]
+                         :when (in-bounds? pos)]
+                     (list (get-in board pos) pos)))))
+          (diagonal-up-move [start-pos board]
+            (let [[down-row down-col] (last (take-while in-bounds? (iterate (fn [[r c]] [(inc r) (dec c)]) start-pos)))]
+              (vec (for [i (range 4)
+                         :let [pos [(- down-row i) (+ down-col i)]]
+                         :when (in-bounds? pos)]
+                     (list (get-in board pos) pos)))))
+          (all-moves-from [[ridx cidx :as pos] board]
+            (let [size (count board)
+                  row-level (vec (map-indexed (fn [idx itm] (list itm [ridx idx])) (get board ridx)))
+                  col-level (vec (map-indexed (fn [idx itm] (list (get itm cidx) [idx cidx])) board))
+                  diag-down (diagonal-down-move pos board)
+                  diag-up (diagonal-up-move pos board)]
+
+              (set [row-level col-level diag-down diag-up])))
+          (valid-moves [pos board]
+            (set (for [move (all-moves-from pos board)
+                       :let [color-move (map first move)]
+                       :when (valid? color-move)]
+                   move)))
+          (positions-of-flips [move color]
+            (->> move
+                 (filter (comp #(and (not= % 'e) (not= % color)) first))
+                 (map second)
+                 (set)))
+          (jump-position [move color]
+            (let [move-str (clojure.string/join "" (map first move))
+                  pattern-fwd (re-pattern (str "e*" color "+" (opposite-of color) "+e+"))
+                  pattern-bck (re-pattern (str "e*" (opposite-of color) "+" color "+e+"))]
+              (if (re-matches pattern-fwd move-str)
+                (last (last move))
+                (last (first move)))))]
+
+    (apply merge-with clojure.set/union
+           (for [color-pos (color-positions color board)
+                 move (valid-moves color-pos board)]
+             {(jump-position move color) (positions-of-flips move color)}))))
 
 (is (= {[1 3] #{[1 2]}, [0 2] #{[1 2]}, [3 1] #{[2 1]}, [2 0] #{[2 1]}}
        (analyze '[[e e e e]
                   [e w b e]
                   [e b w e]
                   [e e e e]] 'w)))
-ee
 (is (= {[3 2] #{[2 2]}, [3 0] #{[2 1]}, [1 0] #{[1 1]}}
        (analyze '[[e e e e]
                   [e w b e]
