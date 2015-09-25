1efac6eb6be2ceb8bf104253b57a528c1fb0cfc0
Miro Bezjak <bezjak.miro@gmail.com>
2015-09-25 10:53:28 +0200

    4clojure: 138 - squares squared

--- clojure/4clojure/138.clj
+++ clojure/4clojure/138.clj
@@ -49,35 +49,160 @@
 (is (= [1 2 3 4] (pad [1 2 3 4])))
 
 (defn empty-shape [dim]
-  (vec (repeat dim (apply str (repeat dim \ )))))
+  (vec (repeat dim (vec (repeat dim \ )))))
 
 ;; 1-4   digits => index=0
 ;; 5-16  digits => index=2
 ;; 17-36 digits => index=4
 
-(defn start-pos-y [size]
+(defn start-pos-x [size]
   (let [base (int (Math/sqrt size))
         even-base (square (if (odd? base) (inc base) base))]
     (- (int (Math/sqrt even-base)) 2)))
 
-(is (= 0 (start-pos-y 1)))
-(is (= 0 (start-pos-y 4)))
-(is (= 2 (start-pos-y 9)))
-(is (= 2 (start-pos-y 16)))
+(is (= 0 (start-pos-x 1)))
+(is (= 0 (start-pos-x 4)))
+(is (= 2 (start-pos-x 9)))
+(is (= 2 (start-pos-x 16)))
 
-(defn start-pos)
+(defn start-pos [size]
+  (let [x (start-pos-x size)
+        y (int (/ (shape-dim size) 2))]
+    [x y]))
 
-(defn can-turn?)
+(is (= [0 0] (start-pos 1)))
+(is (= [0 1] (start-pos 4)))
+(is (= [2 3] (start-pos 16)))
 
-(defn place-digit [shape digit]
-  )
+(defn next-pos [[x y] [move-x move-y]]
+  [(+ x move-x) (+ y move-y)])
+
+(defn can-turn? [pos move shape]
+  (= \space (get-in shape (next-pos pos move))))
+
+(is (= true (can-turn? [0 0] [1 1] ["1 "
+                                    "  "])))
+
+(is (= false (can-turn? [0 0] [1 1] ["1 "
+                                     " 2"])))
+
+(defn next-turn [current-direction]
+  (condp = current-direction
+    [ 1  1] [ 1 -1]
+    [ 1 -1] [-1 -1]
+    [-1 -1] [-1  1]
+    [-1  1] [ 1  1]
+    [ 0  0] [ 1  1]))
+
+(defn place [{:keys [shape pos forward turn]} elem]
+  (if (can-turn? pos turn shape)
+    (let [pos-after-move (next-pos pos turn)]
+      {:shape (assoc-in shape pos-after-move elem)
+       :pos  pos-after-move
+       :forward turn
+       :turn (next-turn turn)})
+    (let [pos-after-move (next-pos pos forward)]
+      {:shape (assoc-in shape pos-after-move elem)
+       :pos  pos-after-move
+       :forward forward
+       :turn turn})))
+
+(is (= [[\space \1 \space]
+        [\space \space \2]
+        [\space \space \space]]
+       (:shape (place {:shape [[\space \1 \space]
+                               [\space \space \space]
+                               [\space \space \space]]
+                       :pos [0 1]
+                       :forward [1 1]
+                       :turn [1 1]}
+                      \2))))
+
+(is (= [[\space \1 \space]
+        [\space \space \2]
+        [\space \3 \space]]
+       (:shape (place {:shape [[\space \1 \space]
+                               [\space \space \2]
+                               [\space \space \space]]
+                       :pos [1 2]
+                       :forward [1 1]
+                       :turn [1 -1]}
+                      \3))))
 
 (defn make-shape [start end]
   (let [elems (pad (digits (squares start end)))
-        shape (empty-shape (shape-dim (count elems)))]
-    (reduce place-digit shape elems)))
+        init-shape (empty-shape (shape-dim (count elems)))
+        init-pos (start-pos (count elems))
+        init-state {:shape init-shape :pos init-pos :forward [0 0] :turn [0 0]}
+        final-state (reduce place init-state elems)
+        final-shape (:shape final-state)
+        vector-of-strings (mapv #(apply str %) final-shape)]
+    vector-of-strings))
 
-ee
+(defn make-shape-4clojure [start end]
+  (letfn [(square [x]
+            (* x x))
+          (squares [start end]
+            (take-while #(<= % end) (iterate square start)))
+          (digits [numbers]
+            (vec (mapcat str numbers)))
+          (shape-dim [num-elems]
+            (->> num-elems
+                 (Math/sqrt)
+                 (Math/round)
+                 (* 2)
+                 dec))
+          (pad [ds]
+            (let [size (count ds)
+                  upper-limit (->> size
+                                   (Math/sqrt)
+                                   (Math/ceil)
+                                   (Math/round)
+                                   (square))]
+              (vec (take upper-limit (concat ds (repeat \*))))))
+          (empty-shape [dim]
+            (vec (repeat dim (vec (repeat dim \ )))))
+          (start-pos-x [size]
+            (let [base (int (Math/sqrt size))
+                  even-base (square (if (odd? base) (inc base) base))]
+              (- (int (Math/sqrt even-base)) 2)))
+          (start-pos [size]
+            (let [x (start-pos-x size)
+                  y (int (/ (shape-dim size) 2))]
+              [x y]))
+          (next-pos [[x y] [move-x move-y]]
+            [(+ x move-x) (+ y move-y)])
+          (can-turn? [pos move shape]
+            (= \space (get-in shape (next-pos pos move))))
+          (next-turn [current-direction]
+            (condp = current-direction
+              [ 1  1] [ 1 -1]
+              [ 1 -1] [-1 -1]
+              [-1 -1] [-1  1]
+              [-1  1] [ 1  1]
+              [ 0  0] [ 1  1]))
+          (place [{:keys [shape pos forward turn]} elem]
+            (if (can-turn? pos turn shape)
+              (let [pos-after-move (next-pos pos turn)]
+                {:shape (assoc-in shape pos-after-move elem)
+                 :pos  pos-after-move
+                 :forward turn
+                 :turn (next-turn turn)})
+              (let [pos-after-move (next-pos pos forward)]
+                {:shape (assoc-in shape pos-after-move elem)
+                 :pos  pos-after-move
+                 :forward forward
+                 :turn turn})))]
+    (let [elems (pad (digits (squares start end)))
+          init-shape (empty-shape (shape-dim (count elems)))
+          init-pos (start-pos (count elems))
+          init-state {:shape init-shape :pos init-pos :forward [0 0] :turn [0 0]}
+          final-state (reduce place init-state elems)
+          final-shape (:shape final-state)
+          vector-of-strings (mapv #(apply str %) final-shape)]
+      vector-of-strings)))
+
+
 (is (= (make-shape 2 2) ["2"]))
 
 (is (= (make-shape 2 4) [" 2 "
