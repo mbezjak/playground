a7d0a4f6c9b49b3cfdcf4e10b2d95e67db9e739c
Miro Bezjak <bezjak.miro@gmail.com>
2016-01-12 12:25:13 +0100

    testcheck.js: working example

--- javascript/testcheck.js/using-testcheck.js
+++ javascript/testcheck.js/using-testcheck.js
@@ -2,9 +2,22 @@ require('jasmine-check').install();
 
 describe('using testcheck', function() {
 
-    check.it('accepts an int and a string', [gen.int, gen.string], function(x, y) {
-        expect(x).toEqual(jasmine.any(Number));
-        expect(y).toEqual(jasmine.any(String));
+    check.it('should fail', [gen.int], function(s) {
+        expect(s).toEqual(jasmine.any(String));
+    });
+
+    check.it('should succeed but jasmine will complain about no expectations', [gen.string], function(s) {
+        expect(s).toEqual(jasmine.any(String));
+    });
+
+    check.it('should test some non trivial property', [gen.array(gen.int), gen.strictPosInt], function(array, amount) {
+        var added = array.map(function(x) {
+            return x + amount;
+        });
+        var sumTwo = function(x, y) { return x + y; };
+        var sumArray = function(a) { return a.reduce(sumTwo, 0); };
+
+        expect(sumArray(added)).toBe(sumArray(array) + array.length * amount);
     });
 
 });
