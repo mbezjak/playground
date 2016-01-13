02cf362b09ccb4242b4322110c059d9c8aaf4ab7
Miro Bezjak <bezjak.miro@gmail.com>
2016-01-13 11:26:16 +0100

    quickcheck: current delete impl falsifies balanced property

--- haskell/quickcheck-testing-for-fun-and-profit/using.hs
+++ haskell/quickcheck-testing-for-fun-and-profit/using.hs
@@ -26,8 +26,32 @@ balanced (Fork _ l r) = (d==0 || d==1) && balanced l && balanced r
 weight Null = 0
 weight (Fork _ l r) = 1 + weight l + weight r
 
-make :: [Integer] -> Tree Integer
-make ns = foldl (\h n -> insert n h) empty ns
+good Null = True
+good (Fork _ l r) = weight l <= weight r
 
-prop_invariant ns = invariant (make ns)
-prop_balanced ns = balanced (make ns)
+deleteMin (Fork x l r) = merge l r
+
+merge l Null = l
+merge Null r = r
+merge l r | minElem l <= minElem r = join l r
+          | otherwise              = join r l
+
+join (Fork x l r) h = Fork x r (merge l h)
+
+data Op = Insert Integer | DeleteMin
+        deriving Show
+
+make :: [Op] -> Tree Integer
+make ops = foldl op Null ops
+  where op h (Insert n) = insert n h
+        op Null DeleteMin = Null
+        op h DeleteMin = deleteMin h
+
+prop_invariant ops = invariant (make ops)
+prop_balanced ops = balanced (make ops)
+prop_AllGood ops = good (make ops)
+
+instance Arbitrary Op where
+  arbitrary =
+    frequency [(2, do n <- arbitrary; return (Insert n)),
+               (1, return DeleteMin)]
