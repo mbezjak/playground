28826c857b0a3d9df702af9f1def84b70b71321a
Miro Bezjak <bezjak.miro@gmail.com>
2016-07-04 11:55:04 +0200

    clojure/clojurescript-quick-start: up-to and including releasing

---
+++ clojure/clojurescript-quick-start/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+
+set -o errexit
+
+# prerequisite: download the standalone ClojureScript JAR
+# https://github.com/clojure/clojurescript/wiki/Quick-Start
+
+declare -r file="${1:-build}"
+
+if [[ $file == repl ]]; then
+    rlwrap java -cp "cljs.jar:src" clojure.main repl.clj
+else
+    java -cp "cljs.jar:src" clojure.main "${file}.clj"
+fi
+
+
+exit 0
