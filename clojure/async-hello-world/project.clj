4784e045a0999d7d6a33eb94cb092b50d6374c6c
Miro Bezjak <bezjak.miro@gmail.com>
2016-11-14 11:28:28 +0100

    clojure/async-hello-world: project name is not instaparse

--- clojure/async-hello-world/project.clj
+++ clojure/async-hello-world/project.clj
@@ -1,5 +1,4 @@
-(defproject instaparse "0.1.0-SNAPSHOT"
-  :description "Example using instaparse"
+(defproject async "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.395"]]
   :main ^:skip-aot example.core
