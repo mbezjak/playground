7a11a1eb3d3805f527c807f85213c96aeeddb798
Miro Bezjak <bezjak.miro@gmail.com>
2018-11-05 12:43:17 +0100

    rust: gen PI by Vietes's series

--- rust/pi/src/main.rs
+++ rust/pi/src/main.rs
@@ -1,3 +1,19 @@
+fn gen_pi_viete_procedural() -> f64 {
+    let n = 1000;
+    let mut pi = 1.0;
+    for i in (2..=n).rev() {
+        let mut f: f64 = 2.0;
+        for _j in 1..i {
+            f = 2.0 + f.sqrt();
+        }
+        f = f.sqrt();
+        pi = pi * f / 2.0;
+    }
+    pi = pi * 2f64.sqrt() / 2.0;
+    pi = 2.0 / pi;
+    pi
+}
+
 fn gen_pi_nilakantha_procedural() -> f64 {
     let n = 1000000;
     let mut s = 1.0;
@@ -10,6 +26,7 @@ fn gen_pi_nilakantha_procedural() -> f64 {
 }
 
 fn main() {
-    println!("PI in rust   {:.20}", std::f64::consts::PI);
-    println!("Generated PI {:.20}", gen_pi_nilakantha_procedural());
+    println!("PI in rust       {:.20}", std::f64::consts::PI);
+    println!("PI by Nilakantha {:.20}", gen_pi_nilakantha_procedural());
+    println!("PI by Viete      {:.20}", gen_pi_viete_procedural());
 }
